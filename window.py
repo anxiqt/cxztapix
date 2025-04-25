4e3b909638c5c973deb53a92404cd713499eb8fd
Anx <jlutz234@proton.me>
2025-04-24 20:56:54 -0500

    added window.py to create window with tk

---
+++ window.py
@@ -0,0 +1,14 @@
+#Here we will define the display window for the user and for demonstration purposes#import for window
+from tkinter import *
+
+window = Tk()
+
+#set title, size, color
+window.title("Agent")
+window.geometry("750x600")
+window.config(background="lightBlue")
+
+
+
+
+window.mainloop()
